ce438798741fbf76bdf3471f70b5b68202741ad5
James Yeates <jimibue@gmail.com>
2021-03-09 12:42:59 -0700

    bugs

--- client/src/Viruses.js
+++ client/src/Viruses.js
@@ -1,9 +1,41 @@
-import { Button } from 'semantic-ui-react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const dummyData = [
+  {id:1, name:'vir 1'},
+  {id:2, name:'vir 2'},
+]
 const Viruses =() => {
+    useEffect(()=>{
+      getBugs()
+    },[])
+
+    const [bugs,setBugs] = useState([])
+
+    const getBugs = async () =>{
+       try{
+         let res = await axios.get('/api/bugs')
+         setBugs(res.data)
+
+       } catch(err){
+         alert('error occured look at console')
+       }
+    }
+
+    const renderBugs =()=>{
+      return bugs.map( bug => {
+        return(
+          <div>
+            {bug.name}
+          </div>
+        )
+      })
+    }
+
     return (
         <>
-          <h1>Viruses</h1>
-          <Button>test</Button>
+          <h1>Bugs</h1>
+          {renderBugs()}
         </>
     )
 }
